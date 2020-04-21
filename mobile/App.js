0f2ce861641128a6da028b24b4395daeb6c4060c
Denis Abakumov <denisab85@gmail.com>
2020-04-20 22:36:26 -0500

    mobile: Initial commit

---
+++ mobile/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.0.6:9090/dishes/cuisine/all')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  });
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
